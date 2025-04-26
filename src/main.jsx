3092c4ab7f908dd8015c3567b4b2b2db8d67f73d
shrestha <shresthachakrabortykgp@gmail.com>
2025-04-26 11:58:39 +0530

    minor bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,11 @@ import AuthContextProvider from "./Components/Context/Auth.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  
     <BrowserRouter>
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
